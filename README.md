bc24092e822178d9a6d4dd75041b130213d76b2b
Bad-Robot <mat.roscoe@unb.ca>
2014-07-03 01:56:03 -0300

    Code Compiles Again :) * Removed OpenCV requirements * Removed OpenNI DIR SETS * Started to removed unneccissary includes

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 NaN-Clustering
 ==============
 
-Master Thesis
+This is the source code repository for my masters thesis source code.
+
+# Dependencies
+This project requires the following libraries or packages: 
+
+1. OpenNI 1.5.4
+   
+    ```
+    sudo apt-get install libopenni-dev libopenni-sensor-primesense0 
+    sudo ln -s /usr/lib/pkgconfig/libopenni.pc /usr/local/lib/pkgconfig/openni-dev.pc 
+    ```
+
+2. [PCL Patched PrimeSense Drivers](https://github.com/avin2/SensorKinect/downloads)
+3. [Point Cloud Library (PCL 1.7 Required)](http://pointclouds.org/downloads/source.html)
+ 
+
+
+
+== Installing  == 
+Drivers: To properly set up OpenNI navigate into the /3rdparty/openni/debian folder of the pcl-trunk. Run the make command and then once it is done install the two debian packages using dpkg -i
--- test/NaNClusteringDemo.cpp
+++ test/NaNClusteringDemo.cpp
@@ -1,8 +1,8 @@
 #include <NaNClusterComparator.h>
 #include <HelperFunctions.h>
 
-
 #include <pcl/visualization/cloud_viewer.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
 #include <iostream>
 #include <string>
@@ -12,33 +12,28 @@
 #include <pcl/filters/project_inliers.h>
 
 #include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
+
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+
+#include <pcl/io/png_io.h>
 #include <pcl/io/openni_grabber.h>
 #include <pcl/io/oni_grabber.h>
 #include <pcl/io/pcd_grabber.h>
-#include <pcl/filters/extract_indices.h>
-
-#include <pcl/io/pcd_io.h>
-#include <pcl/io/png_io.h>
-#include <pcl/io/openni_grabber.h>
 
 #include <pcl/common/time.h>
-#include <pcl/visualization/pcl_visualizer.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/features/integral_image_normal.h>
-#include <pcl/segmentation/organized_multi_plane_segmentation.h>
-#include <pcl/segmentation/planar_polygon_fusion.h>
 #include <pcl/common/transforms.h>
+
+#include <pcl/features/integral_image_normal.h>
+
+//#include <pcl/segmentation/organized_multi_plane_segmentation.h>
+//#include <pcl/segmentation/planar_polygon_fusion.h>
 #include <pcl/segmentation/plane_coefficient_comparator.h>
 #include <pcl/segmentation/euclidean_plane_coefficient_comparator.h>
 #include <pcl/segmentation/rgb_plane_coefficient_comparator.h>
-#include <pcl/segmentation/edge_aware_plane_comparator.h>
+//#include <pcl/segmentation/edge_aware_plane_comparator.h>
 #include <pcl/segmentation/euclidean_cluster_comparator.h>
 #include <pcl/segmentation/organized_connected_component_segmentation.h>
-
-#include <opencv/cv.h>
-#include <opencv/cxcore.h>
-#include <opencv/highgui.h>
 
 #define SHOW_FPS 1
 #if SHOW_FPS
@@ -211,47 +206,7 @@ class SimpleOpenNIViewer
 
 int main( int argc, char *argv[] )
 {
-   //std::string incloudfile = argv[1];
-   //std::string outcloudfile = argv[2];
-
    SimpleOpenNIViewer v; 
    v.run(); 
-
-   // Load cloud
-   // pcl::PointCloud<PointT>::Ptr cloud( new pcl::PointCloud<PointT> );
-   // pcl::io::loadPCDFile( incloudfile.c_str (), *cloud );
-
-   // std::cerr << "PointCloud Width: " << cloud->width << " PointCloud Height: " << cloud->height << "\n"; 
-   // std::cerr << "PointCloud is Organized: " << cloud->isOrganized() << std::endl; 
-   // std::cerr << "PointCloud Contains NaN: " << !cloud->is_dense << std::endl;; 
-
-   // pcl::PointCloud<PointT> outcloud;
-
-   // pcl::NaNClusterComparator<PointT, pcl::Normal, pcl::Label>::Ptr nan_comparator; 
-   // nan_comparator.reset( new pcl::NaNClusterComparator<PointT, pcl::Normal, pcl::Label>() );
-
-   // //Segment Objects
-   // pcl::PointCloud<PointT>::CloudVectorType clusters;
-
-   // nan_comparator->setInputCloud( cloud );
-   // pcl::PointCloud<pcl::Label> nan_labels;
-   // std::vector<pcl::PointIndices> nan_label_indices;
-
-   // pcl::OrganizedConnectedComponentSegmentation<PointT,pcl::Label> nan_segmentation( nan_comparator );
-   // nan_segmentation.setInputCloud( cloud );
-   // nan_segmentation.segment( nan_labels, nan_label_indices );
-
-   // for (size_t i = 0; i < nan_label_indices.size (); i++)
-   // {
-   //    if( nan_label_indices[i].indices.size() > 50 )
-   //    {
-   //       pcl::PointCloud<PointT> cluster;
-   //       pcl::copyPointCloud( *cloud, nan_label_indices[i].indices,cluster );
-   //       clusters.push_back( cluster );
-   //    }
-   // }
-
-   // PCL_INFO( "Got %d nan clusters!\n", clusters.size() );
-
    return (0);
 }
